067ebe6575995481520898d322407ef34293d69c
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-05-10 16:13:36 -0700

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,135 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+
+    function CreateNote (noteContents) {
+
+        console.log('this is ' + noteContents)
+
+        fetch( '/api/note',
+                    {
+                        method: 'POST',
+                        body: JSON.stringify({ noteContents }),
+                        headers: {'Content-Type': 'application/json'}
+                    }
+                ).then (res => res.text())
+                .then(newUID => {
+                    AddNoteItem (newUID)
+                }).catch (error => {
+                    console.error(error)
+                })
+    }
+    
+    function ReadNote (noteID) {
+        fetch(
+            '/api/note/' + noteID
+        )
+        .then(res => res.text())
+        .then(noteContents => {
+            document.getElementById('note-textarea').value = noteContents
+            document.getElementById('note-button-update').setAttribute('data-note-id', noteID)
+            document.getElementById('note-button-delete').setAttribute('data-note-id', noteID)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+    
+    function AddNoteItem (noteID) {
+        const noteItemButton = document.createElement('button')
+        noteItemButton.textContent = 'Read Note'
+        noteItemButton.addEventListener('click', function(){
+        ReadNote(noteID)
+    })
+    
+        const noteItem = document.createElement('li')
+        noteItem.textContent = noteID
+        noteItem.append(noteItemButton)
+    
+        document.getElementById('notes').append(noteItem)
+    }
+    
+    function UpdateNote (noteID, noteContents) {
+        fetch('/api/note/' + noteID, 
+        {
+            method: 'PUT',
+            body: JSON.stringify({ noteContents }),
+            headers: {'Content-Type': 'application/json'}
+        }
+    
+        ).then (res => res.text())
+        .then(updatedNoteContents => {
+           alert(`Note ${noteID} updated:\n${updatedNoteContents}`)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+    
+    function DeleteNote (noteID) {
+        fetch(
+            '/api/note/' + noteID,
+            {
+                method: 'DELETE'
+            }
+        )
+        .then(res => res.text())
+        .then(deletedNoteContents => {
+            alert(`Note ${noteID} deleted:\n${deletedNoteContents}`)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+    
+    function ShowNoteIDs () {
+        fetch(
+            '/api/notes'
+        )
+        .then(res => res.json())
+        .then(noteIDs => {
+            for (let noteID in noteIDs){
+                AddNoteItem(noteID)
+            }
+        })
+        .catch(error => {
+            console.error(error)
+        })    
+    }
+
+    document.getElementById('new-note-button').addEventListener (
+        'click', function (e) {
+
+            const noteContents = document.getElementById('new-note-content').value 
+            
+            CreateNote (noteContents)
+        
+            console.log(noteContents);
+        }
+    )
+
+    document.getElementById('note-button-update').addEventListener (
+        'click', function (e) {
+            const noteContents = document.getElementById('note-textarea').value
+            const noteID = 
+            e.target.getAttribute('data-note-id')
+
+            if (noteID && !isNaN(noteID)) {
+                UpdateNote (noteID, noteContents)
+            }
+        }
+    )
+
+    document.getElementById('note-button-delete').addEventListener (
+        'click', function (e) {
+            const noteID = 
+            e.target.getAttribute('data-note-id')
+
+            if (noteID && !isNaN(noteID)) {
+                DeleteNote (noteID)
+            }
+        }
+    )
+    
+    ShowNoteIDs();
+    
+})
